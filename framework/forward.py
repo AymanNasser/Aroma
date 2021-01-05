804960dffd1519c86acbb1eb63d216b64dec7112
AymanNasser <aymannasr43@gmail.com>
2021-01-05 18:25:48 +0200

    Returned outputs from backward & forward in layers Modified propagate in forward module
    Signed-off-by: AymanNasser <aymannasr43@gmail.com>

--- framework/forward.py
+++ framework/forward.py
@@ -3,11 +3,19 @@ class Forward:
     """
     Forward propagation module
     """
-    def __init__(self, layers):
+    def __init__(self, layers, model_name):
+        # Model total layers
         self.layers = layers
 
-    def forward(self, X):
-        pass
+        self.model_name = model_name
+
+    def Propagate(self, X):
+        """
+        Propagating the input (X) of dim (batch_size, in_dim) through model layers
+        """
+        for layer in self.layers:
+            X = layer(X)
+        return X
 
 
 
--- framework/layers.py
+++ framework/layers.py
@@ -10,23 +10,35 @@ class Layer:
     # We use *args and **kwargs as an argument when we have no doubt about the number of
     # arguments we should pass in a function.
     def __init__(self, *args, **kwargs):
-        # Initializing params for saving & retrieving model weights & biases
-        self.params = Parameters()
         # Initializing cache for intermediate results
         self.cache = {}
-        # Initializing grad for backward prop.
-        self.grad = {}
+        # Initializing grads for backward prop.
+        self.grads = {}
+        # Retrieving model name
+        self.model_name = kwargs['model_name']
+        # Initializing params for saving & retrieving model weights & biases
+        self.params = Parameters.get_model(self.model_name)
 
     def __call__(self, *args, **kwargs):
-        raise NotImplementedError
+        out = self.forward(*args, **kwargs)
+        # self.backward(*args, **kwargs)
+        return out
 
     def __init_weights(self, *args, **kwargs):
         raise NotImplementedError
 
     def forward(self, *args, **kwargs):
+        """
+        Forward pass of the function. Calculates the output value and the
+        gradient at the input as well.
+        """
         raise NotImplementedError
 
     def backward(self, *args, **kwargs):
+        """
+        Backward pass. Computes the local gradient at the input value
+        after forward pass.
+        """
         raise NotImplementedError
 
     def calc_grad(self, *args, **kwargs):
@@ -72,14 +84,16 @@ class Linear(Layer):
         assert Z.shape[0] == (W.shape[0], A_prev.shape[1])
 
         # Caching W & b
-        self.params.cache['W' + str(self.layer_num)] = W
-        self.params.cache['b' + str(self.layer_num)] = b
+        # self.params.cache['W' + str(self.layer_num)] = W
+        # self.params.cache['b' + str(self.layer_num)] = b
 
         # Caching A & Z for grads calc.
         self.cache['A' + str(self.layer_num)] = A_prev
         self.cache['Z' + str(self.layer_num)] = Z
 
-    def backward(self, Y):
+        return Z
+
+    def backward(self, A_prev, Y):
         """
         Backward pass for linear layer
 
@@ -93,12 +107,11 @@ class Linear(Layer):
 
         Return: None
         """
-        A_prev = self.cache['A' + str(self.layer_num-1)]
         A = self.cache['A' + str(self.layer_num)]
-        W = self.cache['W' + str(self.layer_num)]
-        b = self.cache['b' + str(self.layer_num)]
+        W = self.params.get_layer_weights(self.layer_num)
+        b = self.params.get_layer_bias(self.layer_num)
 
-        m = A_prev.shape[1]
+        m = A_prev.shape[1] # training samples
         dZ = (A-Y)
         dW = (1/m) * A_prev * dZ.T
         db = np.squeeze(np.sum(dZ, axis=1, keepdims=True))
@@ -108,13 +121,15 @@ class Linear(Layer):
         assert (dW.shape == W.shape)
 
         # Caching grads
-        self.params.cache_grads['dA' + str(self.layer_num -1)] = dA_prev
-        self.params.cache_grads['dW' + str(self.layer_num )] = dW
-        self.params.cache_grads['db' + str(self.layer_num)] = db
+        self.grads['dA' + str(self.layer_num -1)] = dA_prev
+        self.grads['dW' + str(self.layer_num )] = dW
+        self.grads['db' + str(self.layer_num)] = db
 
         # We should make an activation backwards
         # grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = \
         # linear_backward(sigmoid_backward())
+
+        return dA_prev
 
     def calc_grad(self):
         pass
