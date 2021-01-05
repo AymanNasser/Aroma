602253b1caf95bee69e52e7f0df7896a94f3528e
AhmedMoamen62 <ahmedmoamen138@gmail.com>
2021-01-05 16:16:11 +0200

    add accessing weights matrix

--- framework/parameters.py
+++ framework/parameters.py
@@ -24,55 +24,84 @@ class Parameters:
 
     def __init__(self,model_name):
         self.model_name = model_name
-        self.__weights = {}
+        self.__parameters = {}
 
     def __add_weights(self,W,b,in_dim,out_dim,layer_num):
-        layer_weight = {}
-        w = {'W' + str(i) + str(j): W[i - 1, j - 1] for i in np.arange(start=1, stop=out_dim + 1) for j in np.arange(start=1, stop=in_dim + 1)}
-        b = {'b' + str(i): b[i - 1, 0] for i in np.arange(start=1, stop=out_dim + 1)}
-        layer_weight.update(w)
-        layer_weight.update(b)
+        layer_parameters = {}
+        weights = {'W' + str(i) + str(j): W[i - 1, j - 1] for i in np.arange(start=1, stop=out_dim + 1) for j in np.arange(start=1, stop=in_dim + 1)}
+        bias = {'b' + str(i): b[i - 1, 0] for i in np.arange(start=1, stop=out_dim + 1)}
+        layer_parameters.update(weights)
+        layer_parameters.update(bias)
         layer_name = "Layer " + str(layer_num)
-        self.__weights[layer_name] = {"Weights":layer_weight,"Dimensions":(out_dim,in_dim)}
+        self.__parameters[layer_name] = {"Parameters":layer_parameters, "Dimensions":(out_dim, in_dim)}
 
     def initiate_zeros(self,in_dim,out_dim,layer_num):
         layer_name = "Layer " + str(layer_num)
-        if layer_name in self.__weights.keys():
+        if layer_name in self.__parameters.keys():
             raise AttributeError(layer_name + " is already initialized")
         if layer_num > 1 and self.get_layer_dim(layer_num - 1)[0] != in_dim:
             raise AttributeError("Dimensions conflict in " + layer_name)
+        if layer_num == 0:
+            raise AttributeError("Can't initialize weights for layer zero")
         W = np.zeros((out_dim,in_dim))
         b = np.zeros((out_dim,1))
         self.__add_weights(W,b,in_dim,out_dim,layer_num)
 
     def initiate_random(self,in_dim,out_dim,layer_num):
         layer_name = "Layer " + str(layer_num)
-        if layer_name in self.__weights.keys():
+        if layer_name in self.__parameters.keys():
             raise AttributeError(layer_name + " is already initialized")
         if layer_num > 1 and self.get_layer_dim(layer_num - 1)[0] != in_dim:
             raise AttributeError("Dimensions conflict in " + layer_name)
+        if layer_num == 0:
+            raise AttributeError("Can't initialize weights for layer zero")
         W = np.random.randn(out_dim,in_dim)
         b = np.random.randn(out_dim,1)
         self.__add_weights(W,b,in_dim,out_dim,layer_num)
 
-    def get_layer_weights(self,layer_num):
+    def get_layer_parameters(self,layer_num):
         layer_name = "Layer " + str(layer_num)
-        if layer_name not in self.__weights.keys():
+        if layer_name not in self.__parameters.keys():
             raise RuntimeError(layer_name + " isn't exist")
 
-        return self.__weights[layer_name]["Weights"]
+        return self.__parameters[layer_name]["Parameters"]
+
+    def get_layer_bias(self,layer_num):
+        layer_name = "Layer " + str(layer_num)
+        if layer_name not in self.__parameters.keys():
+            raise RuntimeError(layer_name + " isn't exist")
+
+        parameters_dict = self.__parameters[layer_name]["Parameters"]
+        out_dim, _ = self.__parameters[layer_name]["Dimensions"]
+        bias = np.array([[parameters_dict['b' + str(row)]] for row in np.arange(start=1, stop=out_dim + 1)])
+
+        return bias
+
+    def get_layer_weights(self,layer_num):
+        layer_name = "Layer " + str(layer_num)
+        if layer_name not in self.__parameters.keys():
+            raise RuntimeError(layer_name + " isn't exist")
+
+        parameters_dict = self.__parameters[layer_name]["Parameters"]
+        out_dim, in_dim = self.__parameters[layer_name]["Dimensions"]
+        weights = np.array([[parameters_dict['W' + str(row) + str(col)] for col in np.arange(start=1, stop=in_dim + 1)] for row in np.arange(start=1, stop=out_dim + 1)])
+
+        return weights
 
     def get_layer_dim(self,layer_num):
         layer_name = "Layer " + str(layer_num)
-        if layer_name not in self.__weights.keys():
+        if layer_name not in self.__parameters.keys():
             raise RuntimeError(layer_name + " isn't exist")
 
-        return self.__weights[layer_name]["Dimensions"]
+        return self.__parameters[layer_name]["Dimensions"]
 
 
 w = Parameters("test")
 w.initiate_zeros(4,3,1)
-w.initiate_random(3,7,2)
+w.initiate_random(3,4,2)
 
-print(w.get_layer_weights(1))
+print(w.get_layer_parameters(2))
+print(w.get_layer_weights(2))
+print(w.get_layer_bias(2))
 print(w.get_layer_dim(1))
+print(np.array([[i for i in range(10)],[i for i in range(10)]])[1,2])
