37496dda47de0d69f7d8cf2b82e9202244db53dd
AymanNasser <aymannasr43@gmail.com>
2021-01-14 00:27:14 +0200

    Type: refactor + feat 1. Fixed leaky relu equation 2. Added tanh activation
    Signed-off-by: AymanNasser <aymannasr43@gmail.com>

--- framework/activations.py
+++ framework/activations.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Activation:
     """
     Activation is an abstract Class of any activation function
@@ -30,6 +31,7 @@ class Activation:
         raise NotImplementedError
 
 
+
 class Sigmoid(Activation):
     def forward(self, X):
         return 1/(1 + np.exp(-X))
@@ -40,6 +42,7 @@ class Sigmoid(Activation):
         return sigmoid_grad
 
 
+
 class ReLU(Activation):
     def forward(self, X):
         return X * (X > 0)
@@ -47,6 +50,7 @@ class ReLU(Activation):
     def get_grad(self, X):
         relu_grad = 1 * (X > 0)
         return relu_grad
+
 
 
 class Softmax(Activation):
@@ -62,12 +66,14 @@ class Softmax(Activation):
         S_dir = np.diag(S) - (S_matrix * np.transpose(S_matrix))
         return S_dir
 
+
+
 class LeakyRelU(Activation):
     def __init__(self, negative_slope=0.01):
         self.neg_slope = negative_slope
 
     def forward(self, X):
-        return np.max(0, X) + self.neg_slope * np.min(0, X)
+        return X * (X > 0) + (X * self.neg_slope) * (X <= 0)
 
     def get_grad(self, X):
         l_relu_grad = 1 if X >= 0 else self.neg_slope
@@ -75,13 +81,30 @@ class LeakyRelU(Activation):
 
 
 
-# x = np.array([0.1, 0.5, 0.4])
-#
+class Tanh(Activation):
+    def forward(self, X):
+        return (np.exp(X) - np.exp(-X)) / ((np.exp(X) + np.exp(-X)))
+    
+    def get_grad(self, X):
+        tanh_out = self.forward(X)
+        return (1 - np.power(tanh_out, 2))
+
+
+
+# x = np.array([0.1, -0.5, 0.4])
+
 # W = np.array([[0.1, 0.2, 0.3, 0.4, 0.5],
 #               [0.6, 0.7, 0.8, 0.9, 0.1],
 #               [0.11, 0.12, 0.13, 0.14, 0.15]])
 # Z = np.dot(np.transpose(W), x)
+
 # print(Z)
+
+# a = Tanh()
+# y_hat = a.forward(Z)
+# print(y_hat)
+# print(np.tanh(Z))
+
 # # this line instantiates sigmoid function
 # Y_hat = Sigmoid()
 # # calling the object and passing input calculates both
@@ -89,3 +112,4 @@ class LeakyRelU(Activation):
 # print(Y_hat(Z))
 # # to get the gradient dictionary
 # print(Y_hat.get_grad())
+
