3947dcfd39001078faa0d2264e116f55299294d6
AhmedMoamen62 <ahmedmoamen138@gmail.com>
2021-01-17 02:02:36 +0200

    check loss decreasing

--- framework/test.py
+++ framework/test.py
@@ -6,10 +6,11 @@ import numpy as np
 
 X = np.array([[1],[2],[3]])
 model = Model([Linear(3,10),Sigmoid(),Linear(10,1),Sigmoid()],MSELoss(),model_name="mnist")
-result = model.forward(X)
-loss = model.compute_cost(np.array([[0.5]]),result)
-model.backward()
-model.step()
+for i in range(50):
+    result = model.forward(X)
+    loss = model.compute_cost(np.array([[0.5]]),result)
+    model.backward()
+    model.step()
 
 print(result,loss)
 
