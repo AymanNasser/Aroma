0082f5460b1cbf3b2b33d49a495fabe4cce6e7e6
AymanNasser <aymannasr43@gmail.com>
2021-01-18 03:48:10 +0200

    Type: feat + refactor 1. Modified dataloader to support shuffling, batching 2. Created transform class for converting dataframe to tensor & normalize it 3. Modified download to download to a specific path 4. Added error checking in delete dataset & load data 5. Added partition for batching dataset
    Signed-off-by: AymanNasser <aymannasr43@gmail.com>

---
+++ utils/test.py
@@ -0,0 +1,12 @@
+from data import DataLoader
+import pandas as pd
+import os
+
+# df = pd.read_csv('digit-recognizer/train.csv')
+# print(df.shape)
+
+
+data_loader = DataLoader(split_ratio=0.2, download=False, batch_size=64, shuffle=True) 
+
+X_train, y_train = data_loader.get_train_data()
+print(X_train.shape, y_train.shape)
---
+++ utils/transforms.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Transform:
+    def __init__(self):
+        pass
+
+    def to_tensor(self, data_frame):
+        return data_frame.to_numpy()
+
+
+    def normalize(self, tensor):
+        return tensor / 255.
+
+
+        
