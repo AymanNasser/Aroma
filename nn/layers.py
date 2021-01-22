fe54439815a1c3ca20c22da96f966ccd4e7c1890
Mostafa Abdellatif <mostafaabdellatif.ma@gmail.com>
2021-01-22 22:23:18 +0200

    implemented Conv2Dbackward

--- nn/layers.py
+++ nn/layers.py
@@ -208,8 +208,88 @@ class Conv2D(Layer):
         assert (Z.shape == (n_H, n_W, self.out_channels, m))
         return Z
 
-    def backward(self, *args, **kwargs):
-        pass
+    def backward(self, dZ, A_prev):
+        """
+            Implement the backward propagation for a convolution function
+
+            Arguments:
+            dZ -- gradient of the cost with respect to the output of the conv layer (Z), numpy array of shape (n_H, n_W, n_C,m)
+            cache -- cache of values needed for the conv_backward(), output of conv_forward()
+
+            Returns:
+            dA_prev -- gradient of the cost with respect to the input of the conv layer (A_prev),
+                       numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+            dW -- gradient of the cost with respect to the weights of the conv layer (W)
+                  numpy array of shape (f, f, n_C_prev, n_C)
+            db -- gradient of the cost with respect to the biases of the conv layer (b)
+                  numpy array of shape (1, 1, 1, n_C)
+            """
+
+        ### START CODE HERE ###
+        # Retrieve information from "cache"
+        #(A_prev, W, b, hparameters) = cache
+
+        # Retrieve dimensions from A_prev's shape
+        (n_H_prev, n_W_prev, n_C_prev, m) = A_prev.shape
+
+        # Retrieve dimensions from W's shape
+        W = self.params.get_layer_weights(self.layer_num)
+        (f, f, n_C_prev, n_C) = W.shape
+
+        # Retrieve information from "hparameters"
+        #stride = hparameters["stride"]
+        #pad = hparameters["pad"]
+
+        # Retrieve dimensions from dZ's shape
+        (n_H, n_W, n_C, m) = dZ.shape
+
+        # Initialize dA_prev, dW, db with the correct shapes
+        dA_prev = np.zeros((n_H_prev, n_W_prev, n_C_prev,m))
+        dW = np.zeros((f, f, n_C_prev, n_C))
+        db = np.zeros((1, 1, 1, n_C))
+
+        # Pad A_prev and dA_prev
+        A_prev_pad = A_prev   #zero_pad(A_prev, pad)
+        dA_prev_pad = dA_prev #zero_pad(dA_prev, pad)
+
+        for i in range(m):  # loop over the training examples
+
+            # select ith training example from A_prev_pad and dA_prev_pad
+            a_prev_pad = A_prev_pad[:, :, :, i]
+            da_prev_pad = dA_prev_pad[:, :, :, i]
+
+            for h in range(n_H):  # loop over vertical axis of the output volume
+                for w in range(n_W):  # loop over horizontal axis of the output volume
+                    for c in range(n_C):  # loop over the channels of the output volume
+
+                        # Find the corners of the current "slice"
+                        vert_start = h * self.stride
+
+                        vert_end = vert_start + f
+                        horiz_start = w * self.stride
+
+                        horiz_end = horiz_start + f
+
+                        # Use the corners to define the slice from a_prev_pad
+                        a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+
+                        # Update gradients for the window and the filter's parameters using the code formulas given above
+                        da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[h, w, c,i]
+                        dW[:, :, :, c] += a_slice * dZ[h, w, c, i]
+                        db[:, :, :, c] += dZ[h, w, c, i]
+
+            # Set the ith training example's dA_prev to the unpaded da_prev_pad (Hint: use X[pad:-pad, pad:-pad, :])
+            dA_prev[:, :, :, i] = da_prev_pad[self.padding:-self.padding, self.padding:-self.padding, :]
+        ### END CODE HERE ###
+
+        # Making sure your output shape is correct
+        assert (dA_prev.shape == (n_H_prev, n_W_prev, n_C_prev, m))
+
+        return dA_prev, dW, db
+
+    def padding(self, X):
+        pad_list = [(self.__padding[0],), (self.__padding[1],), (0,), (0,)]
+        return np.pad(X, pad_list, 'constant')
 
 
 class MaxPool2D(Layer):
