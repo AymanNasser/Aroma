0937ca51211e03f4a97bfb22eb6327275f3e7e62
AhmedMoamen62 <ahmedmoamen138@gmail.com>
2021-01-22 20:28:35 +0200

    conv2D forward is tested

--- nn/mnist_train.py
+++ nn/mnist_train.py
@@ -7,18 +7,18 @@ from nn.layers import *
 from nn.losses import *
 
 data_loader = DataLoader(batch_size=64)
-X_train, y_train = data_loader.get_train_data()
+X_train, y_train = data_loader.get_train_data(tensor_shape='4D',H=28,W=28,C=1)
 batches = data_loader.get_batched_data(X_train, y_train)
 
-model = Model([Linear(X_train.shape[0],20),Sigmoid(),Linear(20,15),Sigmoid(),
-               Linear(15,10),Softmax()],CrossEntropyLoss())
-# model = Model([Conv2D(1,4),Sigmoid(),MaxPool2D(),Flatten(),Softmax()],CrossEntropyLoss())
+# model = Model([Linear(X_train.shape[0],20),Sigmoid(),Linear(20,15),Sigmoid(),
+#                Linear(15,10),Softmax()],CrossEntropyLoss())
+model = Model([Conv2D(1,4),Sigmoid(),MaxPool2D(),Flatten(),Softmax()],CrossEntropyLoss())
 epoch = 16
 
 for i in range(epoch):
     for X,Y in tqdm(batches):
         y_pred = model.forward(X)
         loss = model.compute_cost(Y, y_pred)
-        model.backward()
-        model.step()
-    print("Epoch: ", i + 1, "Loss: ", loss)
+        print("Epoch: ", i + 1, "Loss: ", loss)
+        # model.backward()
+        # model.step(learning_rate=0.1)
