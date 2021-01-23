Type: fix + refactor 1. Modified cross entropy loss to NLLLoss 2. Added a term to softmax for num. stability
Signed-off-by: AymanNasser <aymannasr43@gmail.com>

nn/activations.py
@@ -57,7 +57,7 @@ class ReLU(Activation):
 
 class Softmax(Activation):
     def forward(self, X):
-        exp_x = np.exp(X)
+        exp_x = np.exp(X - np.max(X)) # Added np.max(X) for numerical stablility 
         out = exp_x/np.sum(exp_x,axis=0,keepdims=True)
         return out
 
@@ -74,15 +74,14 @@ class LeakyRelU(Activation):
         self.neg_slope = negative_slope
 
     def forward(self, X):
-        debug = X * (X >= 0) + (X * self.neg_slope) * (X < 0)
-        return debug
+        out = X * (X >= 0) + (X * self.neg_slope) * (X < 0)
+        return out
 
     def get_grad(self, X):
         grad = np.copy(X)
         grad[X>=0] = 1.0
         grad[X<0] = self.neg_slope
         return grad
-
 
 
 class Tanh(Activation):

nn/losses.py
@@ -20,7 +20,7 @@ class MSELoss(Loss):
         return 2.0 * (Y_pred - Y) / Y_pred.shape[0]
 
 
-class CrossEntropyLoss(Loss):
+class NLLLoss(Loss):
 
     def calc_loss(self, Y_pred, Y):
         """
@@ -28,8 +28,8 @@ class CrossEntropyLoss(Loss):
         y is labels (num_examples x 1): one hot encode vector
         """
         m = Y.shape[-1]
-        log_likelihood = -np.log(Y_pred[Y, range(m)])
-        loss = np.sum(log_likelihood) / m
+        log_likelihood = np.log(Y_pred[Y, range(m)])
+        loss = - np.sum(log_likelihood) / m
         return loss
 
     def calc_grad(self, Y_pred, Y):
@@ -39,7 +39,7 @@ class CrossEntropyLoss(Loss):
         """
         grad = np.copy(Y_pred)
         m = Y.shape[-1]
-        grad[Y, range(m)] = 1.0
+        grad[Y, range(m)] -= 1.0
         grad = grad / m
         return grad
 