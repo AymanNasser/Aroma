133b4dc8015af294cf1085fc1c373e641cece3d1
AymanNasser <aymannasr43@gmail.com>
2021-01-24 13:45:23 +0200

    Type: feat Modified dataloader to retrieve pandas df
    Signed-off-by: AymanNasser <aymannasr43@gmail.com>

--- utils/test.py
+++ utils/test.py
@@ -9,6 +9,9 @@ import pandas as pd
 
 data_loader = DataLoader(dataset_path=os.getcwd() + '/nn/', split_ratio=0.2, download=False, batch_size=64, shuffle=True) 
 
+df = data_loader.get_pandas_frame()
+print(df.describe())
+
 X_train, y_train = data_loader.get_train_data(tensor_shape='2D', H=28, W=28, C=1)
 X_val, y_val = data_loader.get_validation_data(tensor_shape='4D', H=28, W=28, C=1)
 X_test = data_loader.get_test_data(tensor_shape='4D', H=28, W=28, C=1)
