ef489d18ebe9d9777bcf0c28c491b73a87eb142c
AymanNasser <aymannasr43@gmail.com>
2021-01-26 00:04:29 +0200

    Type: refactor Refacored demo_cnn to train lenet-5
    Signed-off-by: AymanNasser <aymannasr43@gmail.com>

--- src/demo_mnist_cnn.py
+++ src/demo_mnist_cnn.py
@@ -10,6 +10,7 @@ from utils.dataloader import DataLoader
 from eval.evaluation import Evaluation
 from viz.visualization import Visualization
 from utils.transforms import Transform
+from utils.process_tensor import padding
 from tqdm import tqdm
 
 data_loader = DataLoader(batch_size=64, dataset_path="../")
@@ -18,19 +19,37 @@ data_loader = DataLoader(batch_size=64, dataset_path="../")
 X_train, y_train = data_loader.get_train_data(tensor_shape='4D', H=28, W=28, C=1)
 trans = Transform()
 X_train = trans.normalize(X_train)
+
+# Reshaping tensor to 32*32*1
+X_train = padding(X_train, (2,2))
+
+# Batching data
 batches = data_loader.get_batched_data(X_train, y_train)
 
 # Validation
 X_val, Y_val = data_loader.get_validation_data()
 X_val = trans.normalize(X_val)
 
+INPUT_FEATURE = 784
+momentum = 0.9
+learning_rate = 0.001
+EPOCHS = 16
 
-model = Model([Conv2D(1,2, stride=6),
-               ReLU(),
-               Flatten(),
-               Linear(50,10),
-               Softmax()], loss=NLLLoss(), optimizer=Adam(lr=0.01))
-
+# Training LeNet-5
+model = Model(layers=[Conv2D(in_channels=1, out_channels=6, kernel_size=(5,5), stride=(1,1), padding=(0,0), init_type='random'),
+                    Tanh(),
+                    AvgPool2D(kernel_size=2, stride=2, padding=0),
+             
+                    Conv2D(in_channels=6, out_channels=16, kernel_size=(5,5), stride=(1,1), padding=(0,0), init_type='random'), 
+                    Tanh(), 
+                    AvgPool2D(kernel_size=(2,2), stride=(2,2), padding=0),
+             
+                    Flatten(),
+                    Linear(400,120),
+                    Tanh(),
+                    Linear(120,84),
+                    Tanh(),
+                    Softmax()], loss=NLLLoss(), optimizer=SGD(lr=learning_rate, momentum=momentum), model_name='CNN_Model')
 epoch = 16
 
 vis = Visualization()
